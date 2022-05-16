83770b63fc055011b90bd5c148cf05d8c7439dd0
cquentin26200 <qcheron@edenschool.fr>
2022-05-16 15:31:54 +0200

    add function for calculate the sum of number form 1 to 10

--- js/script.js
+++ js/script.js
@@ -8,18 +8,26 @@ printNumberfromOneToTen();
 
 function printOddNumberLessThanOneHundred() {
   for (let i = 100; i > 0; i--) {
-    i % 4 === 0 ? console.log(i) : null
+    i % 4 === 0 ? console.log(i) : null;
   }
 }
 
 printOddNumberLessThanOneHundred();
 
-function printTheMultiplicationTableWithNumberFormOneToTen () {
-    for (let i = 1; i < 11; i++) {
-        for (let o = 1; o < 11; o++) {
-            console.log(`${i} * ${o}`);
-        }
+function printTheMultiplicationTableWithNumberFormOneToTen() {
+  for (let i = 1; i < 11; i++) {
+    for (let o = 1; o < 11; o++) {
+      console.log(`${i} * ${o}`);
     }
+  }
 }
 
 printTheMultiplicationTableWithNumberFormOneToTen();
+
+function calculateTheSumOfNumberFromOneToTen() {
+  for (let i = 1; i < 11; i++) {
+    console.log(`${i} + ${i}`);
+  }
+}
+
+calculateTheSumOfNumberFromOneToTen();
