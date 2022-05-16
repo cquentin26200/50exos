0fb4c932d7f56c7aba90afd77c3b0104daea2ce1
cquentin26200 <77721932+cquentin26200@users.noreply.github.com>
2022-05-16 21:46:39 +0200

    add exo 3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+//1. Print numbers from 1 to 10
+
 function printNumberfromOneToTen() {
-  for (let i = 1; i < 101; i++) {
+  for (let i = 1; i < 11; i++) {
     console.log(i);
   }
 }
 
 printNumberfromOneToTen();
+
+//2. Print the odd numbers less than 100
 
 function printOddNumberLessThanOneHundred() {
   for (let i = 100; i > 0; i--) {
@@ -13,6 +17,18 @@ function printOddNumberLessThanOneHundred() {
 }
 
 printOddNumberLessThanOneHundred();
+
+//3. Print the multiplication table with 7
+
+function printTheMultiplicationTablWithSeven () {
+  for (let i = 1; i < 11; i++) {
+    console.log(`${i} * 7 = ${i*7}`);
+  }
+}
+
+printTheMultiplicationTablWithSeven();
+
+//4. Print all the multiplication tables with numbers from 1 to 10
 
 function printTheMultiplicationTableWithNumberFormOneToTen() {
   for (let i = 1; i < 11; i++) {
@@ -24,6 +40,8 @@ function printTheMultiplicationTableWithNumberFormOneToTen() {
 
 printTheMultiplicationTableWithNumberFormOneToTen();
 
+//5. Calculate the sum of numbers from 1 to 10
+
 function calculateTheSumOfNumberFromOneToTen() {
   for (let i = 1; i < 11; i++) {
     console.log(`${i} + ${i}`);
@@ -31,6 +49,9 @@ function calculateTheSumOfNumberFromOneToTen() {
 }
 
 calculateTheSumOfNumberFromOneToTen();
+
+//6. Calculate 10!
+
 
 function calculateTen () {
     let num = 1;
@@ -41,18 +62,25 @@ function calculateTen () {
 
 calculateTen();
 
+//7. Calculate the sum of even numbers greater than 10 and less than 30
+
 function CalculateTheSumEvenNumbersGreaterThanTenAndLessThanThirty () {
     for (let i = 30; i > 9; i--) {
         if (i % 2 === 0) {
-            console.log(i)
+          console.log(i);
         }
     }
 }
 
 CalculateTheSumEvenNumbersGreaterThanTenAndLessThanThirty();
 
+//8. Create a function that will convert from Celsius to Fahrenheit
+
 function convertCelsiusToFahrenheit (number) {
-  console.log(Math.floor(number * (9 / 5) + 32));
+  return Math.floor(number * (9 / 5) + 32);
 }
 
 convertCelsiusToFahrenheit(34);
+
+//9. Create a function that will convert from Fahrenheit to Celsius
+
